d6231ac14ddd034d3d8d41f5745efff393526192
Jonathan Bilbao <Jonathan Bilbao@192.168.1.145>
2022-09-14 18:28:44 -0600

    Es la clase nueva que me pidieron crear hoy

---
+++ src/main/java/com/mycompany/repositorioejemplo/Animal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.repositorioejemplo;
+
+/**
+ *
+ * @author Jonathan Bilbao
+ */
+public class Animal {
+    public String nombre;
+    public String apellido;
+
+    public Animal(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+    
+    
+}
