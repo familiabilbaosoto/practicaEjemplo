99657009c7fda657ed1587e72f3ab91753b13097
Jonathan Bilbao <Jonathan Bilbao@192.168.1.145>
2022-09-14 18:27:23 -0600

    Esta es mi primera version del proyecto

---
+++ src/main/java/com/mycompany/repositorioejemplo/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.repositorioejemplo;
+
+/**
+ *
+ * @author Jonathan Bilbao
+ */
+public class Persona {
+    public String nombre;
+    public String apellido;
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+    
+    
+}
